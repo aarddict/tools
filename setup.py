2e97edd331f5af1b604e94aee0d9c26f3d702802
Igor Tkach <itkach@aarddict.org>
2009-05-12 13:13:37 -0400

    bump version to 0.7.3.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "aardtools",
-    version = '0.7.2',
+    version = '0.7.3.dev',
     packages = find_packages(),
     entry_points = {
         'console_scripts': ['aardcompile = aardtools.compiler:main',
