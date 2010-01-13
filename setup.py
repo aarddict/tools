e4cf45b73b3861d1091e7013aa72f539cefcfebf
Igor Tkach <itkach@aarddict.org>
2010-01-12 19:36:58 -0500

    Revert back to mwlib 0.12.10 since 0.12.12 breaks named references defined directly in reference list

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ wiki_meta_en = ['wiki/en/copyright.txt',
 
 install_requires = ['aarddict == 0.8.0.dev',
                     'PyICU >= 0.8.1',
-                    'mwlib == 0.12.12']
+                    'mwlib == 0.12.10']
 
 if sys.version_info < (2, 6):
     install_requires += ['simplejson', 'multiprocessing']
